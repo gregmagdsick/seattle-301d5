c8ec1dce6c7627152bfac7f07840611bbc8c6669
muniri92 <MunirIbrahim@Munirs-MacBook-Pro.local>
2016-02-02 15:26:23 -0800

    got the category handler to work properly

--- class-02-jquery-and-events/driver-greg-navigator-munir/scripts/articleView.js
+++ class-02-jquery-and-events/driver-greg-navigator-munir/scripts/articleView.js
@@ -32,6 +32,19 @@ articleView.handleAuthorFilter = function() {
 };
 
 articleView.handleCategoryFilter = function() {
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category = "' + $(this).val() + '"]').show();
+    } else {
+      $('article').show();
+      $('article.template').hide();
+    }
+    $('#author-filter').val('');
+  });
+};
+
+
   // TODO: Just like we do for #author-filter above, we should handle change events on the #category-filter element.
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
@@ -39,8 +52,6 @@ articleView.handleCategoryFilter = function() {
 
 
 
-
-};
 
 
 articleView.handleMainNav = function() {
