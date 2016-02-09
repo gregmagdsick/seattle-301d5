5b38fdb02c824dd7c2ab426d8d362297c1ce9592
Greg Magdsick <greg.magdsick@gmail.com>
2016-02-09 13:32:28 -0800

    cleaned up comments, extra spaces

--- class-07-functional-programming/pair-assignment/kent-greg/scripts/article.js
+++ class-07-functional-programming/pair-assignment/kent-greg/scripts/article.js
@@ -1,5 +1,3 @@
-// TODO: Wrap the entire contents of this file in an IIFE.
-// Pass in to the IIFE a module, upon which objects can be attached for later access.
 (function (module) {
   function Article (opts) {
     this.author = opts.author;
@@ -29,20 +27,11 @@
       return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
     });
 
-    // DONE: Refactor this forEach code, by using a `.map` call instead, since want we are trying to accomplish
-    // is the transformation of one colleciton into another.
-    // rawData.forEach(function(ele) {
-    //   Article.all.push(new Article(ele));
-    // })
     Article.all = rawData.map(function(ele) {
       return new Article(ele);
     });
   };
 
-  // This function will retrieve the data from either a local or remote source,
-  // and process it, then hand off control to the View.
-  // TODO: Refactor this function, so it accepts an argument of a callback function (likely a view function)
-  // to execute once the loading of articles is done.
   Article.fetchAll = function(callback) {
     if (localStorage.rawData) {
       Article.loadAll(JSON.parse(localStorage.rawData));
@@ -51,24 +40,22 @@
     } else {
       $.getJSON('/data/hackerIpsum.json', function(rawData) {
         Article.loadAll(rawData);
-        localStorage.rawData = JSON.stringify(rawData); // Cache the json, so we don't need to request it next time.
+        localStorage.rawData = JSON.stringify(rawData);
         articleView.initIndexPage();
         callback();
       });
     }
   };
 
-  // TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
   Article.numWordsAll = function() {
-    return Article.all.map(function(article) { //mapping array of objects
-      return article.body.match(/\b\w+/g).length //return array of whitespace counts for each article
+    return Article.all.map(function(article) {
+      return article.body.match(/\b\w+/g).length
     })
     .reduce(function(a, b) {
-      return a + b;// Sum up all the values in the collection
+      return a + b;
     });
   };
 
-  // TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names.
   Article.allAuthors = function() {
     return Article.all.map(funciton(article) {
       return article.author;
@@ -76,12 +63,8 @@
     .reduce(function(a, b) {
       if (a.indexOf(b) < 0) a.push(b);
       return a;
-    }, []);// Don't forget to read the docs on map and reduce!
+    }, []);
   };
-  //25-30 minutes on this one
-  // var flattened = [[0,1], [2,3], [4,5]].reduce(function(a,b){
-  //   return a.concat(b);
-  // },[]);
 
   Article.numWordsByAuthor = function() {
     // TODO: Transform each author string into an object with 2 properties: One for
