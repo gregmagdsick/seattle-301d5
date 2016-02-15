e5cfd796e557934a3499c8e72b595bd8173d32ab
Greg Magdsick <greg.magdsick@gmail.com>
2016-02-15 11:34:56 -0800

    finished routes.js

--- class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
+++ class-11-spa-routing/pair-assignment/starter-code/scripts/routes.js
@@ -1,4 +1,5 @@
-// TODO: Configure routes for this app with page.js, by registering each URL your app can handle,
-// linked to a a single controller function to handle it:
+page('/',articlesController.index);
+page('/about', aboutController.index);  //aboutController.index denotes the function that will run when first goint to /about
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute
+
+page();
