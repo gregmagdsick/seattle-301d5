da3225a378c139d2ea94f1bda5096ff59da27adb
Greg Magdsick <greg.magdsick@gmail.com>
2016-02-18 13:30:07 -0800

    commented loadById, loadByCategory, loadByAuthor

--- class-14-managing-state/greg-zach-ben/scripts/articleController.js
+++ class-14-managing-state/greg-zach-ben/scripts/articleController.js
@@ -8,6 +8,18 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*This method is called in routes.js by page(/article/:id)
+  This method takes the parameters ctx and next, which manages state and sets
+  the next callback in the chain.
+
+  This method defines the function articleData that sets the article property of
+  the context(ctx) to be equal to the variable article.  Once that is defined,
+  it calls Article.findWhere, which is invoked with 'id', ctx.params.id, and
+  the articleData function as it's parameters this function will perform a SQL
+  SELECT returning a record that matches the id passed into the function. This
+  function will then invoke the articleData function as it's callback, setting
+  ctx.articles to the returned article, following which the next() callback is invoked..
+  */
   articlesController.loadById = function(ctx, next) {
     var articleData = function(article) {
       ctx.articles = article;
@@ -18,6 +30,15 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*
+  This method is called in routes.js by page('/author/:authorName')
+  This method functions almost identically to articlesController.loadById, except
+  that instead of using id and article, you are searching by author, so
+  Article.findWhere will return an array of articles, rather than one article.
+
+  In additon, the parameter ctx.params.authorName.replace('+', ' '), is replacing
+  the '+' with an empty space ' '.
+  */
   articlesController.loadByAuthor = function(ctx, next) {
     var authorData = function(articlesByAuthor) {
       ctx.articles = articlesByAuthor;
@@ -28,6 +49,12 @@
   };
 
   // COMMENT: What does this method do?  What is it's execution path?
+  /*
+  This method is called in routes.js by page('/category/:categoryName').
+  This method functions almost identically to articlesController.loadById, except
+  that instead of using id and article, you are searching by category, so
+  Article.findWhere will return an array of articles, rather than one article.
+  */
   articlesController.loadByCategory = function(ctx, next) {
     var categoryData = function(articlesInCategory) {
       ctx.articles = articlesInCategory;
